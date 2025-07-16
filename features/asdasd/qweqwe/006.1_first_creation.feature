05ae60bf0991eabd780272f5b3a34f01b862f17d
Nicolò Padovani EXT <nicolo.padovani@fairmat.com>
2025-07-16 14:43:01 +0200

    init

---
+++ features/asdasd/qweqwe/006.1_first_creation.feature
@@ -0,0 +1,6 @@
+Feature: [6.1] Case 1 - First Product Creation (At Revision Date)
+
+        Scenario: [6.1] Case 1 - First Product Creation (At Revision Date)
+            Given I am on Playwright home page
+             When I click link "Get started"
+             Then I see in title "Installationfhaodfhalkdsfjhl"
---
+++ playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from '@playwright/test';
+import { defineBddConfig, cucumberReporter } from 'playwright-bdd';
+
+const testDir = defineBddConfig({
+  features: 'features/**/*.feature',
+  steps: 'features/steps/*.ts',
+});
+
+export default defineConfig({
+  testDir,
+  reporter: [
+    cucumberReporter('html', {
+      outputFile: 'cucumber-report/index.html',
+      externalAttachments: true,
+    }),
+    ['html', { open: 'never' }],
+  ],
+  use: {
+    screenshot: 'on-first-failure',
+    trace: 'on',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
